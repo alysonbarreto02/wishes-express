019c708c70c2b6cbecd69a9a1adcab13f897c262
alysonbarreto02 <alysonbarretito2123@gmail.com>
2023-08-08 16:36:20 -0300

    refactor: prettier code for migration

--- src/migrations/1691521650900-default.ts
+++ src/migrations/1691521650900-default.ts
@@ -1,24 +1,47 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class Default1691521650900 implements MigrationInterface {
-    name = 'Default1691521650900'
+  name = "Default1691521650900"
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "adresses" ALTER COLUMN "created_at" SET DEFAULT NOW()`);
-        await queryRunner.query(`ALTER TABLE "adresses" ALTER COLUMN "updated_at" SET DEFAULT NOW()`);
-        await queryRunner.query(`ALTER TABLE "adresses" ALTER COLUMN "deleted_at" SET DEFAULT NOW()`);
-        await queryRunner.query(`ALTER TABLE "combos" ALTER COLUMN "created_at" SET DEFAULT NOW()`);
-        await queryRunner.query(`ALTER TABLE "combos" ALTER COLUMN "updated_at" SET DEFAULT NOW()`);
-        await queryRunner.query(`ALTER TABLE "combos" ALTER COLUMN "deleted_at" SET DEFAULT NOW()`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "adresses" ALTER COLUMN "created_at" SET DEFAULT NOW()`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "adresses" ALTER COLUMN "updated_at" SET DEFAULT NOW()`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "adresses" ALTER COLUMN "deleted_at" SET DEFAULT NOW()`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "combos" ALTER COLUMN "created_at" SET DEFAULT NOW()`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "combos" ALTER COLUMN "updated_at" SET DEFAULT NOW()`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "combos" ALTER COLUMN "deleted_at" SET DEFAULT NOW()`
+    )
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "combos" ALTER COLUMN "deleted_at" DROP DEFAULT`);
-        await queryRunner.query(`ALTER TABLE "combos" ALTER COLUMN "updated_at" DROP DEFAULT`);
-        await queryRunner.query(`ALTER TABLE "combos" ALTER COLUMN "created_at" DROP DEFAULT`);
-        await queryRunner.query(`ALTER TABLE "adresses" ALTER COLUMN "deleted_at" DROP DEFAULT`);
-        await queryRunner.query(`ALTER TABLE "adresses" ALTER COLUMN "updated_at" DROP DEFAULT`);
-        await queryRunner.query(`ALTER TABLE "adresses" ALTER COLUMN "created_at" DROP DEFAULT`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "combos" ALTER COLUMN "deleted_at" DROP DEFAULT`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "combos" ALTER COLUMN "updated_at" DROP DEFAULT`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "combos" ALTER COLUMN "created_at" DROP DEFAULT`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "adresses" ALTER COLUMN "deleted_at" DROP DEFAULT`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "adresses" ALTER COLUMN "updated_at" DROP DEFAULT`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "adresses" ALTER COLUMN "created_at" DROP DEFAULT`
+    )
+  }
 }
