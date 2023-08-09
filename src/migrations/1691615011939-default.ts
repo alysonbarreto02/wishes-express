f33cab0fcfa55716c987dfbe125668a886652730
alysonbarreto02 <alysonbarretito2123@gmail.com>
2023-08-09 18:17:42 -0300

    refactor: prettier code for migration

--- src/migrations/1691615011939-default.ts
+++ src/migrations/1691615011939-default.ts
@@ -1,16 +1,19 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class Default1691615011939 implements MigrationInterface {
-    name = 'Default1691615011939'
+  name = "Default1691615011939"
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "products" ADD "productsId" integer`);
-        await queryRunner.query(`ALTER TABLE "products" ADD CONSTRAINT "FK_4d542270767b10d95aa3ee12968" FOREIGN KEY ("productsId") REFERENCES "restaurants"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "products" ADD "productsId" integer`)
+    await queryRunner.query(
+      `ALTER TABLE "products" ADD CONSTRAINT "FK_4d542270767b10d95aa3ee12968" FOREIGN KEY ("productsId") REFERENCES "restaurants"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    )
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "products" DROP CONSTRAINT "FK_4d542270767b10d95aa3ee12968"`);
-        await queryRunner.query(`ALTER TABLE "products" DROP COLUMN "productsId"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "products" DROP CONSTRAINT "FK_4d542270767b10d95aa3ee12968"`
+    )
+    await queryRunner.query(`ALTER TABLE "products" DROP COLUMN "productsId"`)
+  }
 }
