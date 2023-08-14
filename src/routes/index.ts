2116ea1b1faad82f5a9344fe15792af9c839267f
alysonbarreto02 <alysonbarretito2123@gmail.com>
2023-08-14 19:32:45 -0300

    feat: adding route Address

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
+import { AddressController } from "@/controllers/AddressController"
 import { RestaurantController } from "@/controllers/RestaurantController"
+
 import { Router } from "express"
 
 const routes = Router()
 
 routes.post("/create-restaurant", new RestaurantController().create)
 routes.get("/list-restaurants", new RestaurantController().list)
+routes.post("/create-address", new AddressController().create)
 
 export default routes
