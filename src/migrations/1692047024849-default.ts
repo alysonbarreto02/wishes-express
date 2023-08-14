5f992a0dfc1179ed0cd5f770a38adff3678c25a6
alysonbarreto02 <alysonbarretito2123@gmail.com>
2023-08-14 18:04:46 -0300

    refactor: prettier code for migration

--- src/migrations/1692047024849-default.ts
+++ src/migrations/1692047024849-default.ts
@@ -1,14 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class Default1692047024849 implements MigrationInterface {
-    name = 'Default1692047024849'
+  name = "Default1692047024849"
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "restaurants" ADD "cpf_cnpj" numeric NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "restaurants" ADD "cpf_cnpj" numeric NOT NULL`
+    )
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "restaurants" DROP COLUMN "cpf_cnpj"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "restaurants" DROP COLUMN "cpf_cnpj"`)
+  }
 }
